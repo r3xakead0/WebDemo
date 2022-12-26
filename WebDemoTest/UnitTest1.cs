797236163c1ddc253b79dbf52346e124439f9903
Afu Tse <solapa@gmail.com>
2022-12-26 12:21:43 -0400

    Add Test

---
+++ WebDemoTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace WebDemoTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Console.WriteLine("Demo Test Case");
+        }
+    }
+}
